ab8491ede053a76460a2158b2ae5fa12b9afa677
kmallah <kmallah987@gmail.com>
2022-07-15 11:43:08 +0100

    get user by id

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ import users from "../db/users.js";
 
 const usersRouter = express.Router();
 
-// get all users
+// GET ALL USERS
+
 usersRouter.get("/", function (req, res) {
   const usersData = {
     success: true,
@@ -11,5 +12,33 @@ usersRouter.get("/", function (req, res) {
   };
   res.json(usersData);
 });
+
+
+// GET A USER BY ID 
+
+usersRouter.get('/:id', function (req, res) {
+  const searchId = req.params.id;
+  let searchedUsers = {};
+  for (let i=0; i<users.length; i++){
+      if (Number(searchId) === users[i].id){
+          searchedUsers = users[i];
+          break;
+      }   
+  }
+  const responseObject = {
+      success: true,
+      payload: searchedUsers
+  };
+  res.json(responseObject);
+});
+
+
+
+
+
+
+
+
+
 
 export default usersRouter;
