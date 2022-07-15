5abf05740bec9d52008ad3dddb3c5de2d4b8777c
MumtazO <mumtazob@gmail.com>
2022-07-15 16:53:59 +0100

    cors package added, so you need to npm i

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import usersRouter from "./routes/users.js";
+import cors from 'cors';
 
 const app = express();
 const Port = 3000;
 
 //midleware for req with body
 app.use(express.json())
+app.use(cors());
 
 app.use("/users", usersRouter);
 
