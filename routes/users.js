f4c7f317a69e8370858c5ef99473fb7ddc8404e0
MumtazO <mumtazob@gmail.com>
2022-07-15 13:33:58 +0100

    attempt at creating new user

--- routes/users.js
+++ routes/users.js
@@ -13,32 +13,45 @@ usersRouter.get("/", function (req, res) {
   res.json(usersData);
 });
 
+// GET A USER BY ID
 
-// GET A USER BY ID 
-
-usersRouter.get('/:id', function (req, res) {
+usersRouter.get("/:id", function (req, res) {
   const searchId = req.params.id;
   let searchedUsers = {};
-  for (let i=0; i<users.length; i++){
-      if (Number(searchId) === users[i].id){
-          searchedUsers = users[i];
-          break;
-      }   
+  for (let i = 0; i < users.length; i++) {
+    if (Number(searchId) === users[i].id) {
+      searchedUsers = users[i];
+      break;
+    }
   }
   const responseObject = {
-      success: true,
-      payload: searchedUsers
+    success: true,
+    payload: searchedUsers,
   };
   res.json(responseObject);
 });
 
+//CREATE USER
+// assigning the variable body to request the body of our new user(body meaning the data)
+// user.push(body) = this adds the data to our users array
 
+// usersRouter.post("/", function (req, res) {
+//   const body = req.body;
+//   users.push(body);
+//   res.json({ success: true, payload: users });
+// });
 
-
-
-
-
-
-
+usersRouter.post('/', function (req, res){
+  const body = req.body;
+  users.push(body);
+  // const updatedUsers = users;
+console.log(body);
+  const responseObject = {
+    success: true,
+    payload: users
+};
+res.json(responseObject);
+// console.log(users);
+});
 
 export default usersRouter;
